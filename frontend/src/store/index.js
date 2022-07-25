599701b58c79dcf083ccbfd6031bc8f4256fa676
bizyoung93 <bizyoung93@gmail.com>
2022-07-25 09:15:19 +0900

    feat: auth api code 작성 회원가입과 로그인 관련된 api 코드 api폴더의 api파일에 함수로 몰아 넣음 회원가입과 로그인 관련 기능 코드 작성 일반 로그인 기능 구현 완료 일반 회원가입 기능 구현 완료 단, 서버와 직접 신호를 주고받지 않았으므로 코드 수정이 필요할 것

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,8 @@
 import { createStore } from 'vuex'
+import users from './users'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    users,
+  },
 })

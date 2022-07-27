c3fc93e02ba3865a26e02ff9334be133c4ccc81e
Hyelim <hallov012@gmail.com>
2022-07-28 02:50:22 +0900

    feat: navbar responsive 화면 크기에 따른 텍스트 로고 변경 화면 크기에 따른 navbar 항목 위치 변경 화면 크기에 따른 dropdown 크기 변경 index.html에 fontawsome 추가

--- frontend/src/components/MainPage/NavBar.vue
+++ frontend/src/components/MainPage/NavBar.vue
@@ -1,21 +1,27 @@
 <template>
  <nav class="navbar">
-  <div class="name">WeSeePlay</div>
-  <ul>
-    <li class="user-text nav-item"><span>반가워요,<p>{{ username }}</p>님 +</span>
-      <ul class="dropdown-box">
-        <li v-if="!isChangeNickname" @click="onChangeNickname">닉네임 변경</li>
-        <li v-else class="nickname-input">
-          <input type="text" :placeholder="username">
-          <span @click="onChangeNickname">X</span>
-          <button>변경</button>
-        </li>
-        <li>비밀번호 변경</li>
-        <li>회원 삭제</li>
-      </ul>
-    </li>
-    <li class="nav-item"><span>로그아웃</span></li>
-  </ul>
+  <ul class="navbar-content">
+    <div class="logo-box">
+      <div class="name">WeSeePlay</div>
+      <img src="../../assets/logo_nontext.png">
+    </div>
+      <li class="user-text nav-item"><span class="welcome-text">반가워요,<p>{{ username }}</p>님 +</span>
+        <ul class="dropdown-box">
+          <li v-if="!isChangeNickname" @click="onChangeNickname">닉네임 변경</li>
+          <li v-else class="nickname-input">
+            <input type="text" :placeholder="username">
+            <span @click="onChangeNickname">X</span>
+            <button>변경</button>
+          </li>
+          <li>비밀번호 변경</li>
+          <li>회원 삭제</li>
+        </ul>
+      </li>
+      <li class="nav-item logout-box">
+        <span>로그아웃</span>
+        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+      </li>
+    </ul>
    </nav>
 </template>
 

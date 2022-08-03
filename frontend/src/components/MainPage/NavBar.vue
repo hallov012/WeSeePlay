436376d74a2d5342e5550ac8fa28e8698571dbff
bizyoung93 <bizyoung93@gmail.com>
2022-08-03 22:09:55 +0900

    FIX: 로그인과 새로고침 시 닉네임이 갱신되지 않는 부분 수정 localStorage와 store 등에서 저장 문제였습니다

--- frontend/src/components/MainPage/NavBar.vue
+++ frontend/src/components/MainPage/NavBar.vue
@@ -8,7 +8,7 @@
       <li class="user-text nav-item">
         <span class="welcome-text"
           >반가워요,
-          <p>{{ username }}</p>
+          <span> {{ username }}</span>
           님 +</span
         >
         <ul class="dropdown-box">
@@ -37,20 +37,19 @@
 </template>
 
 <script>
-import Swal from "sweetalert2"
-import { ref, reactive } from "vue"
-import { useStore } from "vuex"
-import axios from "axios"
-import api from "@/api/api"
-import { useRouter } from "vue-router"
+import Swal from 'sweetalert2'
+import { ref, reactive, watchEffect } from 'vue'
+import { useStore } from 'vuex'
+import axios from 'axios'
+import api from '@/api/api'
+import { useRouter } from 'vue-router'
 
 export default {
-  name: "NavBar",
+  name: 'NavBar',
   setup() {
-    // const username = ref("WeSeePlay")
     const isChangeNickname = ref(false)
     let credentials = reactive({
-      nicknameInput: "",
+      nicknameInput: '',
     })
 
     const onChangeNickname = function () {
@@ -59,13 +58,18 @@ export default {
 
     const store = useStore()
     const router = useRouter()
-    store.dispatch("fetchMe")
 
-    const username = ref(store.getters.me.userNickname)
-    const userEmail = store.getters.me.userEmail
+    // 수정한 부분
+    const userEmail = ref('')
+    const username = ref('')
+
+    watchEffect(() => {
+      username.value = store.getters.me.userNickname
+      userEmail.value = store.getters.me.userEmail
+    })
 
     const logout = function () {
-      store.dispatch("logout")
+      store.dispatch('logout')
     }
 
     const changeNickname = async function () {
@@ -75,8 +79,8 @@ export default {
         console.log(credentials.nicknameInput)
         if (!credentials.nicknameInput) {
           Swal.fire({
-            icon: "error",
-            text: "닉네임을 입력하세요",
+            icon: 'error',
+            text: '닉네임을 입력하세요',
           })
 
           errorFlag = false
@@ -86,21 +90,21 @@ export default {
             6 <=
             credentials.nicknameInput.replace(
               /[\0-\x7f]|([0-\u07ff]|(.))/g,
-              "$&$1$2"
+              '$&$1$2'
             ).length
           ) ||
             !(
               credentials.nicknameInput.replace(
                 /[\0-\x7f]|([0-\u07ff]|(.))/g,
-                "$&$1$2"
+                '$&$1$2'
               ).length <= 24
             ) ||
             !/^[ㄱ-ㅎ|가-힣|a-z|A-Z|]+$/.test(credentials.nicknameInput))
         ) {
           Swal.fire({
-            icon: "error",
-            title: "닉네임 형식이 잘못되었습니다.",
-            text: "한글 2~8자, 영문 6~24자(6~24 byte 이내)",
+            icon: 'error',
+            title: '닉네임 형식이 잘못되었습니다.',
+            text: '한글 2~8자, 영문 6~24자(6~24 byte 이내)',
           })
           errorFlag = false
         }
@@ -111,7 +115,7 @@ export default {
 
         const response = await axios({
           url: api.users.changeNickname,
-          method: "PATCH",
+          method: 'PATCH',
           headers: store.getters.authHeader,
           data: {
             userEmail: userEmail,
@@ -121,17 +125,17 @@ export default {
 
         if (response.status === 200) {
           username.value = credentials.nicknameInput
-          credentials.nicknameInput = ""
+          credentials.nicknameInput = ''
         }
       } catch (error) {
         if (error.response.status === 404) {
           Swal.fire({
-            icon: "error",
-            text: "존재하지 않는 계정입니다. 다시 시도해 주세요.",
+            icon: 'error',
+            text: '존재하지 않는 계정입니다. 다시 시도해 주세요.',
           })
-          store.dispatch("logout")
+          store.dispatch('logout')
         } else {
-          router.push({ name: "errorpage", params: { errorname: 500 } })
+          router.push({ name: 'errorpage', params: { errorname: 500 } })
         }
       }
     }
@@ -149,5 +153,5 @@ export default {
 </script>
 
 <style scoped>
-@import url("../../assets/mainpage/navbar.css");
+@import url('../../assets/mainpage/navbar.css');
 </style>

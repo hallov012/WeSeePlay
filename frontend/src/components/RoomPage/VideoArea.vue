806168889fcf67daa19b9cf0e62ff6a169cc1e9f
ChanWo Won <wonch30@gmail.com>
2022-08-04 11:01:46 +0900

    add/GameVideoList Game 창에서의 영상 출력 창 구현 중간 완료

--- frontend/src/components/RoomPage/VideoArea.vue
+++ frontend/src/components/RoomPage/VideoArea.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="video-area-case" :class="true ? 'non-side' : 'on-side'">
-    <GameVideo v-if="isGameMode" :users="userList" />
+    <!-- isGameMode가 참이면 GameVideo가 나오게 하고, false라면 MeetingVideo가 나오게 짰어-->
+    <LiarGameVideo v-if="isGameMode" :users="userList" />
     <MeetingVideo v-else :users="userList" />
   </div>
 </template>
 
 <script setup>
-import GameVideo from "./game/liargame/VideoList.vue"
+import LiarGameVideo from "./game/liargame/VideoList.vue"
 import MeetingVideo from "./meeting/VideoList.vue"
 import { ref } from "vue"
 
-const isGameMode = ref(true)
-const userList = ref([])
+const isGameMode = ref(false)
+const userList = ref([]) // Component에 넘겨줄 user list
 
+// 이쪽은 샘플로 넣을 User 수 정하는 거 (최종본에는 지울 내용)
 const customNumber = ref(13)
-
 for (let i = 1; i < customNumber.value; i++) {
   const id = i
   const nickname = String.fromCharCode(i + 64)
-  const onVideo = false
+  const onVideo = true
   userList.value.push({ id, nickname, onVideo })
 }
 </script>
@@ -26,6 +27,7 @@ for (let i = 1; i < customNumber.value; i++) {
 <style>
 .video-area-case {
   padding: 15px 15px 95px 15px;
+  position: relative;
 }
 
 .non-side {
--- frontend/src/components/RoomPage/meeting/VideoItem.vue
+++ frontend/src/components/RoomPage/meeting/VideoItem.vue
@@ -1,26 +1,38 @@
 <template>
-  <div class='video-box'>
-    <p>id: {{ user.id }} / nickname: {{ user.nickname }}</p>
-    <p v-if="user.onVideo">
-        <img src="https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png" style='width: 150px; height: 150px;'>
-    </p>
-    <p v-else>Video가 꺼져있습니다.</p>
+  <div class="case">
+    <div class="video-box column justify-center">
+      <p>id: {{ user.id }} / nickname: {{ user.nickname }}</p>
+      <p v-if="user.onVideo">
+        <img
+          src="https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png"
+          style="width: 150px; height: 150px"
+        />
+      </p>
+      <p v-else>Video가 꺼져있습니다.</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: {
-        user: {
-            type: Object,
-            required: true,
-        }
+  props: {
+    user: {
+      type: Object,
+      required: true,
     },
+  },
 }
 </script>
 
 <style>
+.case {
+  padding: 15px;
+  display: inline-block;
+}
+
 .video-box {
-  border: solid 1px black;
+  background-color: #c9c5f1;
+  height: 100%;
+  border-radius: 10px;
 }
 </style>

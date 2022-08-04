e903e9894e1a1d7086baac0878491f18275f429a
bizyoung93 <bizyoung93@gmail.com>
2022-08-04 17:12:18 +0900

    FEAT: 배포된 회원 API 로컬 상태인 룸 API를 분리했습니다

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,12 @@
-const HOST = 'http://localhost:8080/api/v1'
+// 회원 서버 URL
+// const HOST = 'http://localhost:8080/api/v1'
+const AUTHHOST = 'http://i7a501.p.ssafy.io:8010/api/v1'
+
+// 방 서버 URL
+// Room Server가 배포되면 아래 주소로 바꾸어 끼자
+// const ROOMHOST = "http://i7a501.p.ssafy.io:8011/api/v1"
+
+const ROOMHOST = 'http://localhost:8080/api/v1'
 
 const USERS = '/users'
 
@@ -8,45 +16,50 @@ const ROOMS = '/rooms'
 
 const CERTIFICATION = '/certification'
 
+// 아래 카카오 로그인을 할 때는, 프론트엔드쪽으로 포트 번호를 맞추어야 한다
+// 즉 로컬 서버로 연결할 때는 8081, 배포 서버로 날릴 때는 8080으로 날리자
 const KakaoLogin = 'http://localhost:8081/login/kakao'
+// const KakaoLogin = 'http://localhost:8080/login/kakao'
 
-const kakaoSendToken = 'http://localhost:8080/api/v1/oauth/kakao'
+const kakaoSendToken = 'http://i7a501.p.ssafy.io:8010/api/v1/oauth/kakao'
+
+// const kakaoSendToken = 'http://localhost:8080/api/v1/oauth/kakao'
 
 export default {
   // 회원 관련 API
   users: {
     // 회원가입
-    signup: () => HOST + USERS,
+    signup: () => AUTHHOST + USERS,
 
     // 이메일 체크
     duplicateEmail: (email) =>
-      HOST + USERS + EMAIL + `/duplicate/check?userEmail=${email}`,
+      AUTHHOST + USERS + EMAIL + `/duplicate/check?userEmail=${email}`,
 
-    sendEmail: () => HOST + USERS + EMAIL + CERTIFICATION,
+    sendEmail: () => AUTHHOST + USERS + EMAIL + CERTIFICATION,
 
     verifyEmail: (email) =>
-      HOST + USERS + EMAIL + CERTIFICATION + `/check?userEmail=${email}`,
+      AUTHHOST + USERS + EMAIL + CERTIFICATION + `/check?userEmail=${email}`,
 
     // 내 계정 정보(토큰인증)
-    checkToken: () => HOST + USERS + '/me',
+    checkToken: () => AUTHHOST + USERS + '/me',
 
     // 로그인
-    login: () => HOST + '/auth' + '/login',
+    login: () => AUTHHOST + '/auth' + '/login',
 
     // 닉네임 정보 수정
-    changeNickname: () => HOST + USERS + '/nickname',
+    changeNickname: () => AUTHHOST + USERS + '/nickname',
 
     // 비밀번호 수정
-    changePassword: () => HOST + USERS + '/password',
+    changePassword: () => AUTHHOST + USERS + '/password',
 
     // 임시 비밀번호 발송
-    requireTempPassword: () => HOST + USERS + EMAIL + CERTIFICATION + '/pw',
+    requireTempPassword: () => AUTHHOST + USERS + EMAIL + CERTIFICATION + '/pw',
 
     // 이메일 인증 확인
     getTempPassword: (email) =>
-      HOST + USERS + EMAIL + CERTIFICATION + `/pw/check?userEmail=${email}`,
+      AUTHHOST + USERS + EMAIL + CERTIFICATION + `/pw/check?userEmail=${email}`,
 
-    // 카카오 관련 API
+    /* ---------- 카카오 관련 API ---------- */
 
     // 카카오 redirect uri
     kakaoLogin: () => KakaoLogin,
@@ -54,9 +67,9 @@ export default {
     // 카카오 토큰 백엔드 전송 api => 수정 필요
     kakaoSendToken: () => kakaoSendToken,
   },
-  // ROOM 관련 API
+  /* ---------- ROOM 관련 API ---------- */
   room: {
     // ROOM 생성, 삭제, 조회를 한번에!
-    createRoom: () => HOST + ROOMS,
+    createRoom: () => ROOMHOST + ROOMS,
   },
 }

6d6c2b477490b78087fc58b6710cbabf4030b2f1
bizyoung93 <bizyoung93@gmail.com>
2022-08-04 13:39:39 +0900

    FEAT: sideArea에서 GameArea 완성 아직 버튼에 연결은 안 했습니다

--- frontend/src/components/RoomPage/SideArea.vue
+++ frontend/src/components/RoomPage/SideArea.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="sideArea"></div>
+  <div class="sideArea">
+    <button class="exit"><i class="fa-solid fa-x fa-lg"></i></button>
+    <GameArea />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import GameArea from './sidearea/GameArea.vue'
+</script>
 
 <style scoped>
 @import url('../../../src/assets/roompage/sidearea.css');
---
+++ frontend/src/components/RoomPage/sidearea/GameArea.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 타이틀 -->
+  <div class="title">게임</div>
+  <!-- 본문 -->
+  <div class="game-mainarea">
+    <div class="game-item">
+      <div class="game-title game-text">라이어게임</div>
+      <div class="game-text">
+        당신은 제시어에 대해 사람들에게 설명해야 합니다.<br />
+        하지만 걱정하지 마세요. <br />
+        모두에게 제시어가 공유되니까요.<br />
+        한 사람, 라이어를 제외하면요!<br />
+        누가 거짓말을 하고 있는 라이어일까요?<br />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+@import url('../../../../src/assets/roompage/sidearea.css');
+</style>

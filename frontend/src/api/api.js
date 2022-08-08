3be23b406d16ac4ad041fe7fa28263b15e8743e8
bizyoung93 <bizyoung93@gmail.com>
2022-08-08 16:36:59 +0900

    FEAT:카카오 로그인 기능 완성

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,29 +1,27 @@
 // 회원 서버 URL
-// const AUTHHOST = "http://localhost:8080/api/v1"
-const AUTHHOST = "https://i7a501.p.ssafy.io/api/v1"
+const AUTHHOST = 'http://localhost:8010/api/v1'
+// const AUTHHOST = 'https://i7a501.p.ssafy.io/api/v1'
 
 // 방 서버 URL
 // Room Server가 배포되면 아래 주소로 바꾸어 끼자
 // const ROOMHOST = "http://i7a501.p.ssafy.io:8011/api/v1"
 
-const ROOMHOST = "http://localhost:8080/api/v1"
+const ROOMHOST = 'http://localhost:8080/api/v1'
 
-const USERS = "/users"
+const USERS = '/users'
 
-const EMAIL = "/email"
+const EMAIL = '/email'
 
-const ROOMS = "/rooms"
+const ROOMS = '/rooms'
 
-const CERTIFICATION = "/certification"
+const CERTIFICATION = '/certification'
 
-// 아래 카카오 로그인을 할 때는, 프론트엔드쪽으로 포트 번호를 맞추어야 한다
-// 즉 로컬 서버로 연결할 때는 8081, 배포 서버로 날릴 때는 8080으로 날리자
-// const KakaoLogin = 'http://localhost:8081/login/kakao'
-const KakaoLogin = "http://localhost:8080/login/kakao"
+// 서버에 업로드 할 때, 이거 교체하고 올리자
+const KakaoLogin = 'http://localhost:8080/login/kakao'
+// const KakaoLogin = 'https://i7a501.p.ssafy.io/login/kakao'
 
-const kakaoSendToken = "http://i7a501.p.ssafy.io:8010/api/v1/oauth/kakao"
-
-// const kakaoSendToken = 'http://localhost:8080/api/v1/oauth/kakao'
+// const kakaoSendToken = 'https://i7a501.p.ssafy.io/api/v1/oauth/kakao'
+const kakaoSendToken = 'http://localhost:8010/api/v1/oauth/kakao'
 
 export default {
   // 회원 관련 API
@@ -41,22 +39,22 @@ export default {
       AUTHHOST + USERS + EMAIL + CERTIFICATION + `/check?userEmail=${email}`,
 
     // 내 계정 정보(토큰인증)
-    checkToken: () => AUTHHOST + USERS + "/me",
+    checkToken: () => AUTHHOST + USERS + '/me',
 
     // 로그인
-    login: () => AUTHHOST + "/auth" + "/login",
+    login: () => AUTHHOST + '/auth' + '/login',
 
     // 닉네임 정보 수정
-    changeNickname: () => AUTHHOST + USERS + "/nickname",
+    changeNickname: () => AUTHHOST + USERS + '/nickname',
 
     // 비밀번호 수정
-    changePassword: () => AUTHHOST + USERS + "/password",
+    changePassword: () => AUTHHOST + USERS + '/password',
 
     // 이메일 체크(비밀번호 찾기)
-    sendEmailForPW: () => AUTHHOST + USERS + EMAIL + CERTIFICATION + "/pw",
+    sendEmailForPW: () => AUTHHOST + USERS + EMAIL + CERTIFICATION + '/pw',
 
     verifyEmailForPW: () =>
-      AUTHHOST + USERS + EMAIL + CERTIFICATION + "/pw/check",
+      AUTHHOST + USERS + EMAIL + CERTIFICATION + '/pw/check',
 
     // verifyEmailForPW: (email) =>
     //   AUTHHOST + USERS + EMAIL + CERTIFICATION + `/pw/check?userEmail=${email}`,
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,44 +1,44 @@
-import Swal from "sweetalert2"
-import { createRouter, createWebHistory } from "vue-router"
-import StartPage from "../views/StartPage.vue"
-import MainPage from "@/views/MainPage.vue"
-import ErrorPage from "@/views/ErrorPage.vue"
-import RoomPage from "@/views/RoomPage.vue"
+import Swal from 'sweetalert2'
+import { createRouter, createWebHistory } from 'vue-router'
+import StartPage from '../views/StartPage.vue'
+import MainPage from '@/views/MainPage.vue'
+import ErrorPage from '@/views/ErrorPage.vue'
+import RoomPage from '@/views/RoomPage.vue'
 
-import KakaoLoginPage from "@/views/KakaoLoginPage.vue"
+import KakaoLoginPage from '@/views/KakaoLoginPage.vue'
 // eslint-disable-next-line
-import store from "@/store"
+import store from '@/store'
 
 const routes = [
   {
-    path: "/",
-    name: "startpage",
+    path: '/',
+    name: 'startpage',
     component: StartPage,
   },
   {
-    path: "/lounge",
-    name: "mainpage",
+    path: '/lounge',
+    name: 'mainpage',
     component: MainPage,
   },
   {
-    path: "/room/:roomID",
-    name: "roompage",
+    path: '/room/:roomID',
+    name: 'roompage',
     component: RoomPage,
   },
   // 보내는 에러 종류에 따라서 띄우는 에러를 다양하게 함
   {
-    path: "/error/:errorname",
-    name: "errorpage",
+    path: '/error/:errorname',
+    name: 'errorpage',
     component: ErrorPage,
   },
   {
-    path: "/login/kakao",
-    name: "kakaologin",
+    path: '/login/kakao',
+    name: 'kakaologin',
     component: KakaoLoginPage,
   },
   {
-    path: "/:pathMatch(.*)*",
-    redirect: "/error/404",
+    path: '/:pathMatch(.*)*',
+    redirect: '/error/404',
   },
 ]
 
@@ -51,19 +51,27 @@ router.beforeEach((to, from, next) => {
   // 실제 서비스용 navguard
   const { isLoggedin } = store.getters
   // 디버깅용 변수 선언
-  // const isLoggedin = true
 
-  // 스타트페이지가 아닌 곳으로 접근하고, 로그인이 되어 있지 않을 때,
-  if (to.name !== "startpage" && !isLoggedin) {
-    Swal.fire({
-      icon: "warning",
-      text: "로그인이 필요한 페이지입니다!",
-    })
-    next({ name: "startpage" })
-  } else if (to.name === "startpage" && isLoggedin) {
-    next({ name: "mainpage" })
-  } else {
-    next()
+  const authRequiredPages = ['roompage', 'mainpage']
+
+  // 로그인 된 경우
+  if (isLoggedin) {
+    if (to.name === 'startpage') {
+      next({ name: 'mainpage' })
+    } else {
+      next()
+    }
+    // 로그인 안 된 경우
+  } else if (!isLoggedin) {
+    if (authRequiredPages.includes(to.name)) {
+      Swal.fire({
+        icon: 'warning',
+        text: '로그인이 필요한 페이지입니다!',
+      })
+      next({ name: 'startpage' })
+    } else {
+      next()
+    }
   }
 })
 

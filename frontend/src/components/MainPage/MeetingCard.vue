a776e4cc1bfcdb4e43574b1ca3b7dc9073bbb5a5
Hyelim <hallov012@gmail.com>
2022-08-09 11:29:42 +0900

    fix/room_card_info 참여 인원 수 호스트 포함으로 출력되도록 설정

--- frontend/src/components/MainPage/MeetingCard.vue
+++ frontend/src/components/MainPage/MeetingCard.vue
@@ -1,48 +1,40 @@
 <template>
   <div id="card-box" class="flex flex-center">
-      <div id="card-container" @click="icon = true">
-        <div id="card-head">
-   
-          <span id="card-title">
-            {{info.title}}
-          </span>
-          <i
-            id="isprivate-icon"
-            class="fa-solid fa-lock"
-            v-show="info.isPrivate"
-          ></i>   
-        </div>
-
-        <div id="card-host" class="col-12">
-          host: {{info.hostNickname}}
-        </div>
-        
-        <div class="col-12 flex justify-end items-center">
-          <span id="people-number">
-            &nbsp; {{info.joinUsers.length}}명/12명
-          </span>
-        </div>
+    <div id="card-container" @click="icon = true">
+      <div id="card-head">
+        <span id="card-title">
+          {{ info.title }}
+        </span>
+        <i
+          id="isprivate-icon"
+          class="fa-solid fa-lock"
+          v-show="info.isPrivate"
+        ></i>
       </div>
+
+      <div id="card-host" class="col-12">host: {{ info.hostNickname }}</div>
+
+      <div class="col-12 flex justify-end items-center">
+        <span id="people-number"> &nbsp; {{ userNum }}명/12명 </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  components: {
-  },
-  props:{
+  components: {},
+  props: {
     info: Object,
   },
-  setup(){
+  setup(props) {
+    const userNum = props.info.joinUsers.length + 1
 
-  }
-
+    return { userNum }
+  },
 }
 </script>
 
 <style scoped>
-
-@import '../../assets/mainpage/card.css';
+@import "../../assets/mainpage/card.css";
 </style>
-

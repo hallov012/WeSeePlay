b326e79d861359eb938af1f45aadc5362dc61b65
bizyoung93 <bizyoung93@gmail.com>
2022-08-13 17:05:00 +0900

    FIX: 로그인 페이지에서 레이아웃이 이상하게 나오는 부분을 수정했습니다

--- frontend/src/components/StartPage/FirstCarousel.vue
+++ frontend/src/components/StartPage/FirstCarousel.vue
@@ -1,28 +1,28 @@
 <template>
-   <div class="q-mt-md text-center text-black">
-      <div class="overlay__inner first-carousel">
-        <div class="text-box">
-          <h1 class="overlay__title">
-            함께하는 즐거운 시간,
-            <span class="text-gradient">WeSeePlay</span>에서 즐기던가
-          </h1>
-          <p class="overlay__description">
-            저희 서비스는 말이죠웅 수빱뚜삐두바 뚜비두비두비 뚜비두비두비두비 두비두다~
-            저희 서비스는 말이죠웅 수빱뚜삐두바 뚜비두비두비 뚜비두비두비두비 두비두다~
-            저희 서비스는 말이죠웅 수빱뚜삐두바 뚜비두비두비 뚜비두비두비두비 두비두다~
-          </p>
-        </div>
-        <img class="logo-img" src="../../assets/logo.png">
+  <div class="q-mt-md text-center text-black">
+    <div class="overlay__inner first-carousel">
+      <div class="text-box">
+        <h1 class="overlay__title">
+          함께하는 즐거운 시간,
+          <span class="text-gradient">WeSeePlay</span>에서<br />
+          <span>즐기던가</span>
+        </h1>
+        <p class="overlay__description">
+          저희 서비스는 말이죠웅 수빱뚜삐두바 뚜비두비두비 뚜비두비두비두비
+          두비두다~ 저희 서비스는 말이죠웅 수빱뚜삐두바 뚜비두비두비
+          뚜비두비두비두비 두비두다~ 저희 서비스는 말이죠웅 수빱뚜삐두바
+          뚜비두비두비 뚜비두비두비두비 두비두다~
+        </p>
       </div>
+      <img class="logo-img" src="../../assets/logo.png" />
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "FirstCarousel"
+  name: "FirstCarousel",
 }
 </script>
 
-<style>
-
-</style>
+<style></style>

e5277ff2c2f46384c2a1954c830f3134bb316468
ChanWo Won <wonch30@gmail.com>
2022-08-16 23:45:31 +0900

    fix

--- frontend/src/views/RoomPage.vue
+++ frontend/src/views/RoomPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <TopBar v-if="true" :isHost="isHost" />
+  <TopBar v-if="isGameMode !== 1" :isHost="isHost" />
   <div class="main-area">
     <VideoArea
       :isSide="Boolean(isSide)"

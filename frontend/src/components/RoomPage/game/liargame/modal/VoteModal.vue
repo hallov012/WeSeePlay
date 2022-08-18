583599d889373428ffb9f6fbe7f6820d3a47c495
Hyelim <hallov012@gmail.com>
2022-08-18 17:57:50 +0900

    Fit: vote_modal_btn 투표 참여자 목록 버튼 css 오류 수정

--- frontend/src/components/RoomPage/game/liargame/modal/VoteModal.vue
+++ frontend/src/components/RoomPage/game/liargame/modal/VoteModal.vue
@@ -3,20 +3,20 @@
     <div class="modal-window">
       <h6>의심되는 참가자를 지목해주세요!</h6>
       <div id="user-vote-box">
-        <button
-          v-for="(user, key) in userInfo"
-          :key="key"
-          class="overlay__btn user-item"
-          @click="check(user, key)"
-          :id="user"
-        >
-          <div class="name-box">
+        <button v-for="(user, key) in userInfo" :key="key" class="user-item">
+          <div
+            class="name-box overlay__btn"
+            @click="check(user, key)"
+            :id="user"
+          >
             <span class="nickname-text">{{ user }}</span>
             <span class="order-text">발표자 {{ key + 1 }}</span>
           </div>
         </button>
       </div>
-      <button class="overlay__btn skip-btn" @click="clearCheck">skip</button>
+      <div class="user-item">
+        <button class="overlay__btn skip-btn" @click="clearCheck">skip</button>
+      </div>
       <div class="round-time-bar" data-style="smooth" style="--duration: 10">
         <div></div>
       </div>
@@ -37,7 +37,7 @@ export default {
     let userInfo = [...props.nicknameList]
 
     const check = function (user, key) {
-      const checkBtns = document.querySelectorAll(".user-item")
+      const checkBtns = document.querySelectorAll(".name-box")
       const skipBtn = document.querySelector(".skip-btn")
       checkBtns.forEach((cb) => {
         cb.classList.remove("active")
@@ -50,7 +50,7 @@ export default {
     }
 
     const clearCheck = function () {
-      const checkBtns = document.querySelectorAll(".user-item")
+      const checkBtns = document.querySelectorAll(".name-box")
       const skipBtn = document.querySelector(".skip-btn")
       checkBtns.forEach((cb) => {
         cb.classList.remove("active")
@@ -74,5 +74,10 @@ export default {
 
 .modal-window {
   height: auto;
+}
+
+.round-time-bar {
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
 }
 </style>
